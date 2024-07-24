952c11109be4c62ce9b7e742ef4d0e000b3556a8
devonwarren <devon.warren@gmail.com>
2024-07-24 10:23:31 -0400

    add nvim config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,25 @@
-# aliases
-alias g=git
-alias k=kubectl
-alias kns=kubens
-alias kctx=kubectl
+set fish_greeting ""
+
+# nvim setup
+if command -qv nvim 
+    alias vim nvim
+    alias vi nvim 
+    set -gx EDITOR nvim
+end
+
+# git setup
+if command -qv git
+    alias g="git"
+    alias ga="git add"
+    alias gc="git commit"
+end
+
+# kubernetes setup
+if command -qv kubectl
+    alias k="kubectl"
+    alias kns="kubens"
+    alias kctx="kubectx"
+end
 
 
 # initialize starship prompt
